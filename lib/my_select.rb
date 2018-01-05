39c8ab3e0030e7fc2f442497e9a078405c754996
York Wang <yw544@nyu.edu>
2018-01-05 17:50:27 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  array = []
+  while i < collection.length
+    if yield(collection[i])
+      array << collection[i]
+end
+i += 1
+end
+array
 end
