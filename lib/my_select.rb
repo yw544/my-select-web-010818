d5f75bcd103cc8d2407321db73f6ff891639b1c3
York Wang <yw544@nyu.edu>
2018-01-05 17:48:24 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
+  i = 0
+  array = []
+  while i < collection.length
+    if yield(collection[i])
+      array << collection[i]
+end
+i += 1
+end
  # your code here!
 end
